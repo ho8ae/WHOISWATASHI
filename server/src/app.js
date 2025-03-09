992b0968712155ab454029d5de56867d90ec6a4c
ho8ae <xogh2242@gmail.com>
2025-03-09 16:00:22 +0900

    [#3]chore: app.js server.js 라우트 변경

---
+++ server/src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+// Express 앱 초기화
+const app = express();
+
+// 기본 미들웨어
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+
+// 라우트 등록
+const categoryRoutes = require('./category/category.routes');
+app.use('/api/categories', categoryRoutes);
+
+// 기본 경로
+app.get('/', (req, res) => {
+  res.json({ message: '쇼핑몰 API 서버가 실행 중입니다.' });
+});
+
+module.exports = app;
