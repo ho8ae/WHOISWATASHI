bc473f82c3401c5c75d7420110deef8583e95051
ho8ae <xogh2242@gmail.com>
2025-03-10 22:53:27 +0900

    [#5]add: 비밀번호 재설정 테이블 추가

---
+++ server/prisma/migrations/20250310131527_add_password_reset_model/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "password_resets" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires_at" TIMESTAMP(3) NOT NULL,
+    "is_used" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "password_resets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "password_resets_token_key" ON "password_resets"("token");
+
+-- AddForeignKey
+ALTER TABLE "password_resets" ADD CONSTRAINT "password_resets_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
