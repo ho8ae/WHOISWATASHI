7f1772c39272b1af76305f92f6a8eabc7f7e0bad
ho8ae <xogh2242@gmail.com>
2025-03-12 21:28:08 +0900

    [#11]add: wishlist 테이블 추가

---
+++ server/prisma/migrations/20250312121811_add_wishlist/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "wishlists" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "product_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "wishlists_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "wishlists_user_id_idx" ON "wishlists"("user_id");
+
+-- CreateIndex
+CREATE INDEX "wishlists_product_id_idx" ON "wishlists"("product_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "wishlists_user_id_product_id_key" ON "wishlists"("user_id", "product_id");
+
+-- AddForeignKey
+ALTER TABLE "wishlists" ADD CONSTRAINT "wishlists_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "wishlists" ADD CONSTRAINT "wishlists_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE CASCADE ON UPDATE CASCADE;
