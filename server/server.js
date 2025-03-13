8098cdd6cbac1e2593a174ba3c954eb8b0e3c542
ho8ae <xogh2242@gmail.com>
2025-03-13 22:25:55 +0900

    [#13]refactor: log update

--- server/server.js
+++ server/server.js
@@ -4,5 +4,5 @@ const app = require('./src/app');  // app.js 가져오기
 // 서버 시작
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
-  console.log(`서버가 포트 ${PORT}에서 실행 중입니다.`);
+  console.log(`WHOISWATHSHI 서버가 포트 ${PORT}에서 실행 중입니다.\n개발 화이팅입니다.`);
 });
