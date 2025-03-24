3f8ba7c9acc3cd4bd257478fe79b9a0961e40fcf
LOW <126559845+ho8ae@users.noreply.github.com>
2025-03-24 19:30:51 +0900

    [#17]chore: tailwind 설정 및 Header,Footer (#19)
    * [#17]chore: 초기 설정 및 라우트 설정,SCSS 설정
    
    * [#17]docs: README.md update
    
    * [#17]build : SASS 삭제 및 tailwind CSS 설치
    
    * [#17]build : tailwind 및 vite install complete
    
    * [#17]feat: tailwind 웹 퍼블리싱 Header,footer

---
+++ front/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container w-full mx-auto p-4">
+      <div className="">
+        <div>아마 여기에 들어가야 함 scroll action 할 예정</div>
+        <div>그치?</div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
